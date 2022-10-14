8da2a8b5f257369c6f66b7577a704b9546e94c21
webreflection <andrea.giammarchi@gmail.com>
2022-10-14 13:36:29 +0200

    Fix missing jsx expression container

--- esm/index.js
+++ esm/index.js
@@ -1,174 +1,42 @@
-const JSX_ANNOTATION_REGEX = /\*?\s*@jsx\s+([^\s]+)/;
-const JSX_FRAG_ANNOTATION_REGEX = /\*?\s*@jsxFrag\s+([^\s]+)/;
-const JSX_INTERPOLATION_ANNOTATION_REGEX = /\*?\s*@jsxInterpolation\s+([^\s]+)/;
-
 export default ({types: t}) => {
-  let pragma = '', pragmaFrag = '', pragmaPrefix = '', pragmaInterpolation = '';
-
-  const interpolation = () => {
-    return (
-      pragmaInterpolation ||
-      ((pragmaPrefix || 'React') + '.interpolation')
-    );
-  };
-
   return {
     visitor: {
-      Program: {
-        enter(_, state) {
-          const {file: {ast: {comments}}} = state;
-          if (comments) {
-            for (const comment of comments) {
-              if (JSX_ANNOTATION_REGEX.test(comment.value)) {
-                pragma = RegExp.$1;
-                [pragmaPrefix] = pragma.split('.');
-              }
-              else if (JSX_FRAG_ANNOTATION_REGEX.test(comment.value))
-                pragmaFrag = RegExp.$1;
-              else if (JSX_INTERPOLATION_ANNOTATION_REGEX.test(comment.value))
-                pragmaInterpolation = RegExp.$1;
-            }
-          }
-        }
-      },
-      FunctionDeclaration(path) {
-        if (path.node.id.name === '_extends' && path.parentPath.type === 'Program') {
-          console.warn(
-            '\x1b[1mWARNING\x1b[0m: _extends override is not supported.\n',
-            '        Use `{"useSpread": true}` option.'
-          );
-        }
-      },
-      SpreadElement(path) {
-        const {parentPath} = path.parentPath;
-        if (parentPath && parentPath.isCallExpression()) {
-          const name = getCalleeName(parentPath.node.callee);
-          if (
-            name === pragma ||
-            name === (pragma || 'React.createElement')
-          ) {
-            const {callee} = path.parentPath.node;
-            if (callee && getCalleeName(callee) === interpolation())
-              return;
-            path.parentPath.replaceWith(
-              t.inherits(
-                t.callExpression(
-                  toMemberExpression(interpolation()),
-                  [path.parentPath.node]
-                ),
-                path.parentPath
-              )
-            );
-          }
-        }
-      },
-      JSXExpressionContainer(path) {
-        const {expression} = path.node;
-        path.node.expression = t.callExpression(
-          toMemberExpression(interpolation()),
-          [expression]
-        );
-      },
+      JSXExpressionContainer,
       JSXElement(path) {
+        // just return to see above message logged
+        // return;
         if (!path.parentPath.isJSX() && !path.parentPath.isCallExpression()) {
           const openingPath = path.get('openingElement');
-          const attributes = openingPath.get('attributes');
+          for (const node of path.node.children) {
+            if (node.type === 'JSXExpressionContainer')
+              JSXExpressionContainer({node});
+          }
           const callExpr = t.callExpression(
-            toMemberExpression((pragma || 'React.createElement') + '``'),
+            toMemberExpression('React.createElement``'),
             [
               getTag(openingPath),
-              attributes.length ?
-                t.objectExpression(attributes.reduce(accumulateAttribute, [])) :
-                t.nullLiteral(),
+              t.nullLiteral(),
               ...t.react.buildChildren(path.node),
             ]
           );
           path.replaceWith(t.inherits(callExpr, path.node));
         }
-      },
-      JSXFragment(path) {
-        if (!path.parentPath.isJSX() && !path.parentPath.isCallExpression()) {
-          const callExpr = t.callExpression(
-            toMemberExpression((pragma || 'React.createElement') + '``'),
-            [
-              toMemberExpression(pragmaFrag || 'React.Fragment'),
-              t.nullLiteral(),
-              ...t.react.buildChildren(path.node)
-            ]
-          );
-          path.replaceWith(t.inherits(callExpr, path.node));
-        }
       }
     }
   };
 
+  function JSXExpressionContainer(path) {
+    const {expression} = path.node;
+    path.node.expression = t.callExpression(
+      toMemberExpression('React.interpolation'),
+      [expression]
+    );
+  }
   /*!
    * (c) Babel Project - MIT License
    * The following code has been taken and slightly readapted from
    * https://github.com/babel/babel/blob/0058b7fef4d028d2826645b28e3b448517cd979d/packages/babel-plugin-transform-react-jsx/src/create-plugin.ts
    */
-  function accumulateAttribute(array, attribute) {
-    if (t.isJSXSpreadAttribute(attribute.node)) {
-      const arg = attribute.node.argument;
-      if (t.isObjectExpression(arg)) {
-        array.push(...arg.properties);
-      } else {
-        array.push(t.spreadElement(arg));
-      }
-      return array;
-    }
-
-    const value = convertAttributeValue(
-      attribute.node.name.name !== "key"
-        ? attribute.node.value || t.booleanLiteral(true)
-        : attribute.node.value,
-    );
-
-    if (attribute.node.name.name === "key" && value === null) {
-      throw attribute.buildCodeFrameError(
-        'Please provide an explicit key value. Using "key" as a shorthand for "key={true}" is not allowed.',
-      );
-    }
-
-    if (
-      t.isStringLiteral(value) &&
-      !t.isJSXExpressionContainer(attribute.node.value)
-    ) {
-      value.value = value.value.replace(/\n\s+/g, " ");
-      delete value.extra?.raw;
-    }
-
-    const {name} = attribute.node;
-    if (t.isJSXNamespacedName(name)) {
-      attribute.node.name = t.stringLiteral(
-        `${name.namespace.name}:${name.name.name}`
-      );
-    } else if (t.isValidIdentifier(name.name, false)) {
-      name.type = "Identifier";
-    } else {
-      attribute.node.name = t.stringLiteral(name.name);
-    }
-
-    array.push(
-      t.inherits(
-        t.objectProperty(
-          attribute.node.name,
-          value,
-        ),
-        attribute.node,
-      ),
-    );
-    return array;
-  }
-
-  function convertAttributeValue(node) {
-    return t.isJSXExpressionContainer(node) ?
-      t.callExpression(
-        toMemberExpression(interpolation()),
-        [node.expression]
-      ) :
-      node;
-  }
 
   function convertJSXIdentifier(node, parent) {
     if (t.isJSXIdentifier(node)) {
@@ -190,10 +58,6 @@ export default ({types: t}) => {
       return t.stringLiteral(`${node.namespace.name}:${node.name.name}`);
 
     return node;
-  }
-
-  function getCalleeName({object, property, name}) {
-    return name || [object.name, property.name].join('.');
   }
 
   function getTag(openingPath) {
--- test/input.jsx
+++ test/input.jsx
@@ -1,18 +1,7 @@
-/** @jsx test.createElement *//** @jsxFrag test.Fragment *//** @jsxInterpolation test.interpolation */
-
-function Component({ className, props, others }) {
-  return (
-    <>
-      <div id="my-div" className={className}>
-        <>
-          <span />
-          OK
-        </>
-        <p color={color} label="f&quot;o" hidden={Math.random() < .5} />
-      </div>
-      <Component id="my-component" className={className} {...props} {...others}>
-        {[<p a="a" b={Math.random() < .5} />]}
-      </Component>
-    </>
-  );
-}
+const div = (
+  <div>
+    A
+    {Math.random() < .5 ? <a /> : <b />}
+    B
+  </div>
+);
