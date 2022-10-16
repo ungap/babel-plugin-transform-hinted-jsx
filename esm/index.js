1d1811573163f9ba7f1c9a60ade61384f7142ccf
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-10-16 12:02:02 +0200

    Transform in Program visitor to avoid relying on plugins order

--- esm/index.js
+++ esm/index.js
@@ -14,22 +14,65 @@ export default ({types: t}) => {
 
   return {
     visitor: {
-      Program: {
-        enter(_, state) {
-          const {file: {ast: {comments}}} = state;
-          if (comments) {
-            for (const comment of comments) {
-              if (JSX_ANNOTATION_REGEX.test(comment.value)) {
-                pragma = RegExp.$1;
-                [pragmaPrefix] = pragma.split('.');
-              }
-              else if (JSX_FRAG_ANNOTATION_REGEX.test(comment.value))
-                pragmaFrag = RegExp.$1;
-              else if (JSX_INTERPOLATION_ANNOTATION_REGEX.test(comment.value))
-                pragmaInterpolation = RegExp.$1;
+      Program(path, state) {
+        const {file: {ast: {comments}}} = state;
+        if (comments) {
+          for (const comment of comments) {
+            if (JSX_ANNOTATION_REGEX.test(comment.value)) {
+              pragma = RegExp.$1;
+              [pragmaPrefix] = pragma.split('.');
             }
+            else if (JSX_FRAG_ANNOTATION_REGEX.test(comment.value))
+              pragmaFrag = RegExp.$1;
+            else if (JSX_INTERPOLATION_ANNOTATION_REGEX.test(comment.value))
+              pragmaInterpolation = RegExp.$1;
           }
         }
+
+        path.traverse({
+          JSXExpressionContainer(path) {
+            const {expression} = path.node;
+
+            path.node.expression = t.callExpression(
+              toMemberExpression(interpolation()),
+              [expression]
+            );
+          },
+          JSXElement: {
+            exit(path) {
+              if (!path.parentPath.isJSX() && !path.parentPath.isCallExpression()) {
+                const openingPath = path.get('openingElement');
+                const attributes = openingPath.get('attributes');
+                const callExpr = t.callExpression(
+                  toMemberExpression((pragma || 'React.createElement') + '``'),
+                  [
+                    getTag(openingPath),
+                    attributes.length ?
+                      t.objectExpression(attributes.reduce(accumulateAttribute, [])) :
+                      t.nullLiteral(),
+                    ...t.react.buildChildren(path.node),
+                  ]
+                );
+                path.replaceWith(t.inherits(callExpr, path.node));
+              }
+            }
+          },
+          JSXFragment: {
+            exit(path) {
+              if (!path.parentPath.isJSX() && !path.parentPath.isCallExpression()) {
+                const callExpr = t.callExpression(
+                  toMemberExpression((pragma || 'React.createElement') + '``'),
+                  [
+                    toMemberExpression(pragmaFrag || 'React.Fragment'),
+                    t.nullLiteral(),
+                    ...t.react.buildChildren(path.node)
+                  ]
+                );
+                path.replaceWith(t.inherits(callExpr, path.node));
+              }
+            }
+          }
+        })
       },
       FunctionDeclaration(path) {
         if (path.node.id.name === '_extends' && path.parentPath.type === 'Program') {
@@ -59,48 +102,6 @@ export default ({types: t}) => {
                 path.parentPath
               )
             );
-          }
-        }
-      },
-      JSXExpressionContainer(path) {
-        const {expression} = path.node;
-
-        path.node.expression = t.callExpression(
-          toMemberExpression(interpolation()),
-          [expression]
-        );
-      },
-      JSXElement: {
-        exit(path) {
-          if (!path.parentPath.isJSX() && !path.parentPath.isCallExpression()) {
-            const openingPath = path.get('openingElement');
-            const attributes = openingPath.get('attributes');
-            const callExpr = t.callExpression(
-              toMemberExpression((pragma || 'React.createElement') + '``'),
-              [
-                getTag(openingPath),
-                attributes.length ?
-                  t.objectExpression(attributes.reduce(accumulateAttribute, [])) :
-                  t.nullLiteral(),
-                ...t.react.buildChildren(path.node),
-              ]
-            );
-            path.replaceWith(t.inherits(callExpr, path.node));
-          }
-        }
-      },
-      JSXFragment: {
-        exit(path) {
-          if (!path.parentPath.isJSX() && !path.parentPath.isCallExpression()) {
-            const callExpr = t.callExpression(
-              toMemberExpression((pragma || 'React.createElement') + '``'),
-              [
-                toMemberExpression(pragmaFrag || 'React.Fragment'),
-                t.nullLiteral(),
-                ...t.react.buildChildren(path.node)
-              ]
-            );
-            path.replaceWith(t.inherits(callExpr, path.node));
           }
         }
       }
